32fdb5939af315aad42349dac27879d292ecf18b
Will <28814921+wkey1980@users.noreply.github.com>
2020-12-22 13:55:59 +0000

    Create file structure

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Main template file
+ * 
+ * @package aquila
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress Theme</title>
+</head>
+<body>
+    
+</body>
+</html>
